eb3aaa42179116d6fbdd3d573b78a40395b5e407
gaofeng <714496183@qq.com>
2018-04-27 17:42:56 +0800

    add

--- quasar.conf.js
+++ quasar.conf.js
@@ -71,7 +71,8 @@ module.exports = function (ctx) {
         'QTab',
         'QTabPane',
         'QInnerLoading',
-        'QSpinnerPuff'
+        'QSpinnerPuff',
+        'QScrollArea'
 
       ],
       directives: [
@@ -84,6 +85,8 @@ module.exports = function (ctx) {
     },
     // animations: 'all' --- includes all animations
     animations: [
+      'fadeIn',
+      'fadeOut'
     ],
     pwa: {
       cacheExt: 'js,html,css,ttf,eot,otf,woff,woff2,json,svg,gif,jpg,jpeg,png,wav,ogg,webm,flac,aac,mp4,mp3',
--- src/components/list.vue
+++ src/components/list.vue
@@ -9,13 +9,44 @@
     background: rgba(189, 189, 189, 0.5);
   }
 }
+
+.songs {
+  .song {
+    line-height: 20px;
+
+    & + .song {
+      border-top: 1px solid black;
+    }
+
+    .left {
+      width: calc(100% - 48px);
+
+      .song_name {
+        font-size: 16px;
+      }
+
+      .artist_and_album {
+        font-size: 14px;
+        color: grey;
+      }
+    }
+
+    .right {
+    }
+  }
+}
+
+.q-scroll-area {
+  height: calc(100vh - 320px);
+}
 </style>
 
 <template>
   <q-page class="column">
+    <div>{{ selectedSuggestTxt }}</div>
     <div class="column col-12 q-pa-md">
       <q-search :debounce="0" type="text" name="searchSuggest"
-        v-model="searchSuggestTxt" v-stream:keyup="keyup$"
+        v-model="searchSuggestTxt" v-stream:keyup="inputKeyup$"
         v-stream:focus="focus$" v-stream:blur="blur$"></q-search>
       <template v-if="showSuggestions && searchSuggestTxt !== ''">
         <transition appear enter-active-class="animated fadeIn"
@@ -26,7 +57,7 @@
               <q-item v-for="(sItem, i) of suggestionItems"
                 :key="sItem.id" :class="[{ 'selected': i === selectIndex - 1}, 'cursor-pointer']"
                 v-stream:mouseover.native="{ subject: sItemMouseover$, data: { index: i} }"
-                v-stream:click="{ subject: sItemClick$, data: { label: sItem.label } }">
+                v-stream:click.native.stop="{ subject: sItemClick$, data: { label: sItem.label } }">
                 <q-item-main :label="sItem.label"></q-item-main>
               </q-item>
             </template>
@@ -43,13 +74,33 @@
 
     <div class="row col-12">
       <q-tabs align="center" class="full-width">
-        <!-- Tabs - notice slot="title" -->
         <q-tab default slot="title" name="songs"
           icon="library music" />
         <q-tab slot="title" name="album" icon="album"
         />
-        <!-- Targets -->
-        <q-tab-pane name="songs">Tab One</q-tab-pane>
+        <q-tab-pane name="songs" class="q-pa-none">
+          <template>
+            <q-scroll-area>
+              <ul class="column songs">
+                <li v-for="(song, index) of songItems" :key="index"
+                  class="row song">
+                  <div class="column left justify-center q-pa-sm">
+                    <span class="song_name ellipsis">{{ song.name }}</span>
+                    <span class="artist_and_album ellipsis">
+                      {{ song.artists.map(artist => artist.name).join(',')
+                      }} {{ song.artists[0].name
+                      }}·{{ song.album.name
+                      }}
+                    </span>
+                  </div>
+                  <div class="right q-pa-md">
+                    <q-icon name="more vert" />
+                  </div>
+                </li>
+              </ul>
+            </q-scroll-area>
+          </template>
+        </q-tab-pane>
         <q-tab-pane name="album">Tab Two</q-tab-pane>
       </q-tabs>
     </div>
@@ -64,6 +115,7 @@
 import * as R from 'ramda'
 import Rx from 'rxjs/Rx'
 import { startLoading, endLoading } from 'src/utils'
+import { pluck } from 'rxjs/operator/pluck'
 
 const MAX_SUGGESTION_COUNT = 6
 
@@ -75,102 +127,140 @@ export default {
       suggestionItemsloading: false
     }
   },
-  domStreams: ['keyup$', 'focus$', 'blur$', 'sItemMouseover$', 'sItemClick$'],
+  domStreams: [
+    'inputKeyup$',
+    'focus$',
+    'blur$',
+    'sItemMouseover$',
+    'sItemClick$',
+    'sItemKeyup$'
+  ],
   subscriptions() {
-    const getSongs = this.sItemClick$
-      .pluck('data', 'label')
-      .do(console.log)
+    const selectIndex = Rx.Observable.merge(
+      Rx.Observable.of({ value: 1, type: 'init' }),
+      this.inputKeyup$
+        .pluck('event', 'msg', 'keyCode')
+        .map(
+          R.cond([
+            [R.equals(38), R.always({ value: -1, type: 'up' })],
+            [R.equals(40), R.always({ value: 1, type: 'down' })],
+            [
+              R.anyPass([
+                R.both(R.gte(R.__, 65), R.lte(R.__, 90)),
+                R.both(R.gte(R.__, 48), R.lte(R.__, 57)),
+                R.both(R.gte(R.__, 96), R.lte(R.__, 105))
+              ]),
+              R.always({ value: 1, type: 'set' })
+            ],
+            [R.T, R.always(0)]
+          ])
+        )
+        .filter(R.complement(R.equals(0))),
+      this.sItemMouseover$
+        .pluck('data', 'index')
+        .map(R.compose(R.assoc('value', R.__, { type: 'set' }), R.add(1)))
+    ).scan((acc, { value, type }) => {
+      const newAcc = R.defaultTo(acc + value)(acc.value)
+      return R.cond([
+        [R.equals('set'), R.always(value)],
+        [
+          R.T,
+          () =>
+            R.cond([
+              [R.lt(R.__, 1), R.always(MAX_SUGGESTION_COUNT)],
+              [R.gt(R.__, MAX_SUGGESTION_COUNT), R.always(1)],
+              [R.T, R.always(newAcc)]
+            ])(newAcc)
+        ]
+      ])(type)
+    })
+
+    const showSuggestions = Rx.Observable.merge(
+      Rx.Observable.of(false),
+      this.focus$.mapTo(true),
+      this.blur$.mapTo(false)
+    )
+
+    const suggestionItems = this.$watchAsObservable('searchSuggestTxt')
+      .startWith([])
+      .do(() => {
+        this.suggestionItemsloading = true
+      })
+      .debounceTime(400)
+      .pluck('newValue')
+      .distinctUntilChanged()
       .switchMap(txt =>
-        Rx.Observable.fromPromise(
-          this.$musicAPI.get(`/search?keywords=${txt}&type=1`)
+        Rx.Observable.fromPromise(this.$musicAPI.get(`/search?keywords=${txt}`))
+      )
+      // .startWith([])
+      .pluck('data', 'result', 'songs')
+      .map(
+        R.ifElse(
+          R.isNil,
+          R.always([]),
+          R.compose(
+            R.take(MAX_SUGGESTION_COUNT),
+            R.map(({ name, artists, id, ...others }) => ({
+              label: `${name} ${R.compose(R.join(','), R.pluck('name'))(
+                artists
+              )}`,
+              id,
+              others
+            }))
+          )
         )
       )
-      .do(console.log)
-
+      .do(() => {
+        this.suggestionItemsloading = false
+      })
+    // .do(console.log)
     return {
-      suggestionItems: this.$watchAsObservable('searchSuggestTxt')
-        .do(() => {
-          this.suggestionItemsloading = true
+      suggestionItems,
+      showSuggestions,
+      selectIndex,
+      selectedSuggestTxt: Rx.Observable.combineLatest(
+        selectIndex,
+        suggestionItems
+      )
+        .do(console.log)
+        .map(([index, arr]) => arr[index])
+        .do(console.log),
+      songItems: this.sItemClick$
+        .pluck('data', 'label')
+        .merge(
+          this.inputKeyup$
+            .filter(({ event }) => event.msg.keyCode === 13)
+            .mapTo(this.selectedSuggestTxt)
+        )
+
+        .do(label => {
+          this.searchSuggestTxt = label
         })
-        .debounceTime(400)
-        .pluck('newValue')
-        .distinctUntilChanged()
         .switchMap(txt =>
           Rx.Observable.fromPromise(
-            this.$musicAPI.get(`/search/suggest?keywords=${txt}`)
+            this.$musicAPI.get(`/search?keywords=${txt}&type=1`)
           )
         )
-        .startWith([])
-        .map(
-          R.compose(
-            R.ifElse(
-              R.isNil,
-              R.always([]),
-              R.compose(
-                R.take(MAX_SUGGESTION_COUNT),
-                R.map(({ name, artists, id, ...others }) => ({
-                  label: `${name} - ${R.compose(R.join(','), R.pluck('name'))(
-                    artists
-                  )}`,
-                  id,
-                  others
-                }))
-              )
-            ),
-            R.prop('songs'),
-            R.prop('result'),
-            R.prop('data')
-          )
-        )
-        .do(() => {
-          this.suggestionItemsloading = false
-        })
-        .do(console.log),
-      selectIndex: Rx.Observable.merge(
-        Rx.Observable.of({ value: 1, type: 'init' }),
-        this.keyup$
-          .pluck('event', 'msg', 'keyCode')
-          .map(
-            R.cond([
-              [R.equals(38), R.always({ value: -1, type: 'up' })],
-              [R.equals(40), R.always({ value: 1, type: 'down' })],
-              [
-                R.anyPass([
-                  R.both(R.gte(R.__, 65), R.lte(R.__, 90)),
-                  R.both(R.gte(R.__, 48), R.lte(R.__, 57)),
-                  R.both(R.gte(R.__, 96), R.lte(R.__, 105))
-                ]),
-                R.always({ value: 1, type: 'set' })
-              ],
-              [R.T, R.always(0)]
-            ])
-          )
-          .filter(R.complement(R.equals(0))),
-        this.sItemMouseover$
-          .pluck('data', 'index')
-          .map(R.compose(R.assoc('value', R.__, { type: 'set' }), R.add(1)))
-      ).scan((acc, { value, type }) => {
-        const newAcc = acc.value || acc + value
-        return R.cond([
-          [R.equals('set'), R.always(value)],
-          [
-            R.T,
-            () =>
-              R.cond([
-                [R.lt(R.__, 1), R.always(MAX_SUGGESTION_COUNT)],
-                [R.gt(R.__, MAX_SUGGESTION_COUNT), R.always(1)],
-                [R.T, R.always(newAcc)]
-              ])(newAcc)
-          ]
-        ])(type)
-      }),
-      showSuggestions: Rx.Observable.merge(
-        Rx.Observable.of(false),
-        this.focus$.mapTo(true),
-        this.blur$.mapTo(false)
-      )
+        .pluck('data', 'result', 'songs')
+        .map(R.defaultTo([]))
+        // .do(console.log)
     }
   },
+  // computed: {
+  //   selectedSuggestTxt() {
+  //     console.log('this.selectIndex', this.selectIndex)
+  //     console.log('this.suggestionItems', this.suggestionItems)
+  //     console.log(
+  //       'R.nth(this.selectIndex - 1, this.suggestionItems).label',
+  //       R.nth(this.selectIndex - 1, this.suggestionItems).label
+  //     )
+  //     if (this.selectIndex && this.suggestionItems) {
+  //       console.log('wtf')
+  //       return R.nth(this.selectIndex - 1, this.suggestionItems).label
+  //     }
+  //     return ''
+  //   }
+  // },
   created() {},
   methods: {}
 }
